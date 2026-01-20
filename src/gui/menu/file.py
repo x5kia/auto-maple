e2c4b41d1f0f345bf242d72f05ae9f94057dcbe1
x5kia <31186957+x5kia@users.noreply.github.com>
2026-01-20 10:15:41 +0800

    Update file.py
    中文介面

--- src/gui/menu/file.py
+++ src/gui/menu/file.py
@@ -37,3 +37,61 @@ class File(MenuBarItem):
         self.entryconfig('載入腳本', state=tk.NORMAL)
 
     # ... (其餘功能邏輯如 _new_routine 等)
+
+    @staticmethod
+    @utils.run_if_disabled('\n[!] Cannot create a new routine while Auto Maple is enabled')
+    def _new_routine():
+        if config.routine.dirty:
+            if not askyesno(title='New Routine',
+                            message='The current routine has unsaved changes. '
+                                    'Would you like to proceed anyways?',
+                            icon='warning'):
+                return
+        config.routine.clear()
+
+    @staticmethod
+    @utils.run_if_disabled('\n[!] Cannot save routines while Auto Maple is enabled')
+    def _save_routine():
+        file_path = asksaveasfilename(initialdir=get_routines_dir(),
+                                      title='Save routine',
+                                      filetypes=[('*.csv', '*.csv')],
+                                      defaultextension='*.csv')
+        if file_path:
+            config.routine.save(file_path)
+
+    @staticmethod
+    @utils.run_if_disabled('\n[!] Cannot load routines while Auto Maple is enabled')
+    def _load_routine():
+        if config.routine.dirty:
+            if not askyesno(title='Load Routine',
+                            message='The current routine has unsaved changes. '
+                                    'Would you like to proceed anyways?',
+                            icon='warning'):
+                return
+        file_path = askopenfilename(initialdir=get_routines_dir(),
+                                    title='Select a routine',
+                                    filetypes=[('*.csv', '*.csv')])
+        if file_path:
+            config.routine.load(file_path)
+
+    @staticmethod
+    @utils.run_if_disabled('\n[!] Cannot load command books while Auto Maple is enabled')
+    def _load_commands():
+        if config.routine.dirty:
+            if not askyesno(title='Load Command Book',
+                            message='Loading a new command book will discard the current routine, '
+                                    'which has unsaved changes. Would you like to proceed anyways?',
+                            icon='warning'):
+                return
+        file_path = askopenfilename(initialdir=os.path.join(config.RESOURCES_DIR, 'command_books'),
+                                    title='Select a command book',
+                                    filetypes=[('*.py', '*.py')])
+        if file_path:
+            config.bot.load_commands(file_path)
+
+
+def get_routines_dir():
+    target = os.path.join(config.RESOURCES_DIR, 'routines', config.bot.command_book.name)
+    if not os.path.exists(target):
+        os.makedirs(target)
+    return target
